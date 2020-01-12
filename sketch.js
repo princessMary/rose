ab61794fd8c674a35291ce03ca5223e7e483ed66
Maria <43845187+princessMary@users.noreply.github.com>
2020-01-12 11:50:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,31 @@
-var kitty;
-let img;
-
-function preload() {
-  img = loadImage("n2.png");
-}
+var x = 300;
+var y = 300;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-	loadImage('n1.png"', img1 => {
-		image(img, 0, 0);
-	});
-	background(100);
+	createCanvas(windowWidth, windowHeight);
+	background(255);
 }
-function draw() {
 
-	image(img, 0, 0);
-	image(img2, 0, 0);
-	ellipse(20,20, mouseX, mouseY);
+function draw() {
+  if (mouseIsPressed){
+    var rar = random(-40,40);
+    var ran = random(-50,50);
+  	noStroke();
+  	fill(x/2,y/2,random(50,255), random(0,255));
+  	ellipse(mouseX+rar, mouseY+ran, 20, 20);
+    x = mouseX
+    y = mouseY
+  	x = x + random(-30,30);
+  	y = y + random(-30,30);
+
+  	if (x>width){
+  		x = x-100;
+  	} else if(x<0){
+  		x = random(100,300);
+  	} else if(y > height){
+  	  y = y-100;
+  	} else if (y< 0){
+  		y = random(10,100);
+  	}
+  }
 }
